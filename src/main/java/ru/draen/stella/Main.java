cfdfd5b683cd36b819a57472dab309bc872e6348
ThatDraenGuy <haykin.oleg@rambler.ru>
2026-03-15 21:21:11 +0300

    error messages fixes

--- src/main/java/ru/draen/stella/Main.java
+++ src/main/java/ru/draen/stella/Main.java
@@ -3,6 +3,7 @@ package ru.draen.stella;
 
 import org.antlr.v4.runtime.CharStreams;
 import ru.draen.stella.typecheck.TypeChecker;
+import ru.draen.stella.typecheck.exceptions.TypeCheckException;
 
 import java.io.IOException;
 
@@ -10,8 +11,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         try {
             new TypeChecker().checkTypes(CharStreams.fromStream(System.in));
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
+        } catch (TypeCheckException e) {
+            System.err.println(e.report());
             System.exit(1);
         }
     }
--- src/main/java/ru/draen/stella/typecheck/TypeChecker.java
+++ src/main/java/ru/draen/stella/typecheck/TypeChecker.java
@@ -4,16 +4,11 @@ import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import ru.draen.stella.generated.StellaLexer;
 import ru.draen.stella.generated.StellaParser;
-import ru.draen.stella.typecheck.exceptions.TypeCheckException;
 
 public class TypeChecker {
     public void checkTypes(CharStream input) {
         StellaLexer lexer = new StellaLexer(input);
         StellaParser parser = new StellaParser(new CommonTokenStream(lexer));
-        try {
-            parser.start_Program().accept(new TypeCheckVisitor(new TypeCheckRegistry()));
-        } catch (TypeCheckException e) {
-            throw new RuntimeException(e.report());
-        }
+        parser.start_Program().accept(new TypeCheckVisitor(new TypeCheckRegistry()));
     }
 }
